5239333fc1abd118dab0449141605db54f54aef1
Darshan Gada <gadadarshan@gmail.com>
2020-12-19 21:19:13 +0530

    Fix form height issue

--- src/pages/request.js
+++ src/pages/request.js
@@ -5,15 +5,16 @@ import { ReactTypeformEmbed } from 'react-typeform-embed';
 
 const Request = () => {
     return (
-        <Layout title="Hello">
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    fontSize: '20px',
-                }}>
-                <ReactTypeformEmbed url="https://gadadarshan.typeform.com/to/eP0xEgyX" />
+        <Layout title="Request API Key">
+            <div>
+                <ReactTypeformEmbed url="https://gadadarshan.typeform.com/to/eP0xEgyX" style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '80%',
+                    overflow: 'hidden'
+                }}></ReactTypeformEmbed>
             </div>
         </Layout >
     );
