a1a8fc412e0b422bd14586b2596059976fe52479
Darshan Gada <gadadarshan@gmail.com>
2021-01-27 11:59:44 +0530

    Update google form

--- src/pages/request.js
+++ src/pages/request.js
@@ -7,13 +7,13 @@ const Request = () => {
     return (
         <Layout title="Request API Key">
             <div>
-                <ReactTypeformEmbed url="https://gadadarshan.typeform.com/to/eP0xEgyX" style={{
-                    position: 'absolute',
+                <ReactTypeformEmbed url="https://docs.google.com/forms/d/e/1FAIpQLSciOf_227-3pKGKJok6TM0QF2PZhSgfQwy-F-bQaBj0OUgMmA/viewform?embedded=true" style={{
+                    position: 'unset',
                     top: 0,
                     left: 0,
                     width: '100%',
-                    height: '80%',
-                    overflow: 'hidden'
+                    height: '550px',
+                    overflow: 'visible'
                 }}></ReactTypeformEmbed>
             </div>
         </Layout >
