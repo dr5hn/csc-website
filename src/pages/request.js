b64269b43ae191072e67047a9392cdc85a048ef4
Darshan Gada <gadadarshan@gmail.com>
2021-02-09 11:59:23 +0530

    Switch to typeform

--- src/pages/request.js
+++ src/pages/request.js
@@ -7,13 +7,21 @@ const Request = () => {
     return (
         <Layout title="Request API Key">
             <div>
-                <ReactTypeformEmbed url="https://docs.google.com/forms/d/e/1FAIpQLSciOf_227-3pKGKJok6TM0QF2PZhSgfQwy-F-bQaBj0OUgMmA/viewform?embedded=true" style={{
+                {/* <ReactTypeformEmbed url="https://docs.google.com/forms/d/e/1FAIpQLSciOf_227-3pKGKJok6TM0QF2PZhSgfQwy-F-bQaBj0OUgMmA/viewform?embedded=true" style={{
                     position: 'unset',
                     top: 0,
                     left: 0,
                     width: '100%',
                     height: '550px',
                     overflow: 'visible'
+                }}></ReactTypeformEmbed> */}
+                <ReactTypeformEmbed url="https://gadadarshan.typeform.com/to/eP0xEgyX" style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '80%',
+                    overflow: 'hidden'
                 }}></ReactTypeformEmbed>
             </div>
         </Layout >
