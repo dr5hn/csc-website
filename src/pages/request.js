6b6ffdf477d0659b40231232680bdaf0354aa3c1
Darshan Gada <darshan@sozodesign.co.uk>
2021-02-24 12:18:17 +0530

    Switch to google form

--- src/pages/request.js
+++ src/pages/request.js
@@ -7,22 +7,22 @@ const Request = () => {
     return (
         <Layout title="Request API Key">
             <div>
-                {/* <ReactTypeformEmbed url="https://docs.google.com/forms/d/e/1FAIpQLSciOf_227-3pKGKJok6TM0QF2PZhSgfQwy-F-bQaBj0OUgMmA/viewform?embedded=true" style={{
+                <ReactTypeformEmbed url="https://docs.google.com/forms/d/e/1FAIpQLSciOf_227-3pKGKJok6TM0QF2PZhSgfQwy-F-bQaBj0OUgMmA/viewform?embedded=true" style={{
                     position: 'unset',
                     top: 0,
                     left: 0,
                     width: '100%',
                     height: '550px',
                     overflow: 'visible'
-                }}></ReactTypeformEmbed> */}
-                <ReactTypeformEmbed url="https://gadadarshan.typeform.com/to/eP0xEgyX" style={{
+                }}></ReactTypeformEmbed>
+                {/* <ReactTypeformEmbed url="https://gadadarshan.typeform.com/to/eP0xEgyX" style={{
                     position: 'absolute',
                     top: 0,
                     left: 0,
                     width: '100%',
                     height: '80%',
                     overflow: 'hidden'
-                }}></ReactTypeformEmbed>
+                }}></ReactTypeformEmbed> */}
             </div>
         </Layout >
     );
